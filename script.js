93c162816a7d4d39cbeb571131961f8a2e44e858
AleMac17 <alemacia17@gmail.com>
2022-10-30 01:44:36 -0300

    Hecha logica del a pestaña, quedo funcional

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let boton1 = document.getElementById("boton1");
 let boton2 = document.getElementById("boton2");
 let boton3 = document.getElementById("boton3");
+let lineas1 = document.getElementById("lineas-grid-1");
+let lineas2 = document.getElementById("lineas-grid-2");
+let lineas3 = document.getElementById("lineas-grid-3");
+
 
 
 function cambiarPestana(boton){
@@ -8,14 +12,23 @@ function cambiarPestana(boton){
         case boton1: boton1.classList.toggle ("botonpresionado");
         boton2.classList.remove ("botonpresionado")
         boton3.classList.remove ("botonpresionado")
+        lineas1.classList.remove("no-visible");
+        lineas2.classList.add("no-visible");
+        lineas3.classList.add("no-visible");
         break;
         case boton2: boton2.classList.toggle ("botonpresionado");
         boton1.classList.remove ("botonpresionado")
         boton3.classList.remove ("botonpresionado")
+        lineas1.classList.add("no-visible");
+        lineas2.classList.remove("no-visible");
+        lineas3.classList.add("no-visible");
         break;
         case boton3: boton3.classList.toggle ("botonpresionado");
         boton1.classList.remove ("botonpresionado")
         boton2.classList.remove ("botonpresionado")
+        lineas1.classList.add("no-visible");
+        lineas2.classList.add("no-visible");
+        lineas3.classList.remove("no-visible");
         break;
         default:console.log("Queondaasdasdasd");       
     }
